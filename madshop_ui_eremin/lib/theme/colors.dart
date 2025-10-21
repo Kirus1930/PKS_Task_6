b5e8467e8cc64103eaaacfec1a2420a2f26e557a
Kirus1930 <k_eryomin00@mail.ru>
2025-10-21 15:38:20 +0300

    Update colors.dart

---
+++ madshop_ui_eremin/lib/theme/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1E88E5);
+  static const accent = Color(0xFFFFC107);
+  static const background = Color(0xFFF5F5F5);
+  static const textPrimary = Color(0xFF333333);
+  static const textSecondary = Color(0xFF666666);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const red = Color(0xFFFF5252);
+  static const grey = Color(0xFFE0E0E0);
+}
