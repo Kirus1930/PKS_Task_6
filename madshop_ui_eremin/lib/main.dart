19015f66537a9030a71b1724cc3020f85e973912
Kirus1930 <k_eryomin00@mail.ru>
2025-10-23 20:13:06 +0300

    Update main.dart

--- madshop_ui_eremin/lib/main.dart
+++ madshop_ui_eremin/lib/main.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
 import 'screens/splash_screen.dart';
+import 'screens/create_account_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/password_screen.dart';
+import 'screens/shop_screen.dart';
+import 'screens/favorites_screen.dart';
+import 'screens/cart_screen.dart';
+import 'models/app_state.dart';
+import 'theme/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => AppState(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MAD Shopping',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: AppColors.background,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColors.white,
-          elevation: 0,
-        ),
-      ),
-      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.light(),
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName: (_) => const SplashScreen(),
+        CreateAccountScreen.routeName: (_) => const CreateAccountScreen(),
+        LoginScreen.routeName: (_) => const LoginScreen(),
+        PasswordScreen.routeName: (_) => const PasswordScreen(),
+        ShopScreen.routeName: (_) => const ShopScreen(),
+        FavoritesScreen.routeName: (_) => const FavoritesScreen(),
+        CartScreen.routeName: (_) => const CartScreen(),
+      },
     );
   }
 }
