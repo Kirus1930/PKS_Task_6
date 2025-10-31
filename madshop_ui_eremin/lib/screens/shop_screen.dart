25abc75eadc91a6539cc3e7f9f166206acc53365
Kirus1930 <k_eryomin00@mail.ru>
2025-10-31 09:25:41 +0300

    Update shop_screen.dart

--- madshop_ui_eremin/lib/screens/shop_screen.dart
+++ madshop_ui_eremin/lib/screens/shop_screen.dart
@@ -3,37 +3,43 @@ import 'package:provider/provider.dart';
 import '../models/app_state.dart';
 import '../widgets/product_card.dart';
 
-class ShopScreen extends StatelessWidget {
+class ShopScreen extends StatefulWidget {
   static const routeName = '/shop';
   const ShopScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShopScreen> createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+    switch (index) {
+      case 0:
+        Navigator.of(context).pushReplacementNamed('/shop');
+        break;
+      case 1:
+        Navigator.of(context).pushReplacementNamed('/favorites');
+        break;
+      case 2:
+        Navigator.of(context).pushReplacementNamed('/cart');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppState>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            const Text('Shop'),
-            const SizedBox(width: 8),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(12)),
-              child: Text('Cart: ${appState.totalItemsInCart}'),
-            )
-          ],
+        title: const Text(
+          'Shop',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        actions: [
-          IconButton(
-              onPressed: () => Navigator.of(context).pushNamed('/favorites'),
-              icon:
-                  Image.asset('assets/images/heart.png', color: Colors.black)),
-          IconButton(
-              onPressed: () => Navigator.of(context).pushNamed('/cart'),
-              icon: Image.asset('assets/images/bag.png', color: Colors.black))
-        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
@@ -46,6 +52,33 @@ class ShopScreen extends StatelessWidget {
           itemCount: appState.products.length,
           itemBuilder: (ctx, i) => ProductCard(product: appState.products[i]),
         ),
+      ),
+
+      // --- Добавляем нижнее меню ---
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        elevation: 8,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Cart',
+          ),
+        ],
       ),
     );
   }
